a7e597abae5adc6b94d8fa23a704f9b353f0721e
mayola <mayola@marketpsychdata.com>
2024-09-23 13:30:13 -0300

    fixed requirements.txt && fixed auth with login and register for new users

--- app.py
+++ app.py
@@ -4,30 +4,32 @@ import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = b'_5#y2L"F4Q8DsasDajwuh12z\n\xec]/'
 
 # Configura CORS para permitir solicitudes desde el frontend
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 # Función de autenticación
-def auth(user_alias, passwd):
+def auth(username, passwd):
     """Define si un usuario puede iniciar sesión o no"""
     try:
         db = mysql.connector.connect(
             host="localhost",
-            user="flask_user",
-            password="1234",
-            database="classpanner",
+            user="root",
+            password="",
+            database="reports",
             port=3306
         )
         cursor = db.cursor(dictionary=True)
-        query = "SELECT user_alias, user_password, user_type FROM users WHERE user_alias = %s"
-        cursor.execute(query, (user_alias,))
+        query = "SELECT username, user_type, password FROM usuarios WHERE username = %s OR password = %s"
+        cursor.execute(query, (username,passwd))
         result = cursor.fetchone()
+        if not result:
+            return None
         cursor.close()
         db.close()
 
-        if result and check_password_hash(result['user_password'], passwd):
+        if check_password_hash(result["password"],passwd):
             return result['user_type']
         else:
             return None
@@ -37,51 +39,50 @@ def auth(user_alias, passwd):
 
 @app.route("/login", methods=["POST"])
 def login():
-    user_alias = request.form.get("user_alias")
-    password = request.form.get("password")
+    username = request.json.get("username")
+    password = request.json.get("password")
 
-    if not user_alias or not password:
-        return {"error": "User alias and password are required"}, 400
-
-    role = auth(user_alias, password)
-    if role:
-        session["user_alias"] = user_alias
-        session["role"] = role
-        return {"message": "Login successful", "role": role}, 200
+    if not username or not password:
+        return {"error": "Ingresa usuario y contraseña"}, 400
+    user = auth(username, password)
+    print(user)
+    if user:
+        session["username"] = username
+        session["user_type"] = user
+        return {"message": "Login successful", "user": user}, 200
     else:
-        return {"error": "Unauthorized"}, 401
+        return {"error": "El usuario o la contraseña son incorrectos."}, 401
 
 @app.route("/register", methods=["POST"])
 def register():
-    user_name = request.form.get("user_name")
-    user_lastname = request.form.get("user_lastname")
-    user_alias = request.form.get("user_alias")
-    user_email = request.form.get("user_email")
-    password = request.form.get("password")
-    role = request.form.get("role")
+    username= request.json.get("username")
+    password= request.json.get("password")
+    birthday= request.json.get("birthday")
+    repassword= request.json.get("repassword")
+    dni= request.json.get("dni")
+    print(request.json)
+    if not username or not birthday or not repassword or not dni:
+        return {"error": "All fields are required"}, 403
 
-    if not user_name or not user_lastname or not user_alias or not user_email or not password or not role:
-        return {"error": "All fields are required"}, 400
-
-    if role not in ['profesor', 'alumno', 'admin', 'director']:
-        return {"error": "Role not allowed"}, 400
+    if password != repassword:
+        return {"error": "Las contraseñas no coinciden"}, 400
 
     hashed_password = generate_password_hash(password)
 
     try:
         db = mysql.connector.connect(
             host="localhost",
-            user="flask_user",
-            password="1234",
-            database="classpanner",
+            user="root",
+            password="",
+            database="reports",
             port=3306
         )
         cursor = db.cursor()
         query = """
-        INSERT INTO users (user_name, user_lastname, user_password, user_email, user_type, user_alias) 
-        VALUES (%s, %s, %s, %s, %s, %s)
+        INSERT INTO usuarios (username, password, dni, nacimiento) 
+        VALUES (%s, %s, %s, %s)
         """
-        cursor.execute(query, (user_name, user_lastname, hashed_password, user_email, role, user_alias))
+        cursor.execute(query, (username, hashed_password, dni, birthday))
         db.commit()
         cursor.close()
         db.close()
@@ -131,6 +132,75 @@ def google_login():
     except mysql.connector.Error as excp:
         print(f"Error registrando usuario: {excp}")
         return {"error": "Error registering user"}, 500
+    
+@app.route("/escuela", methods=["POST", "GET"])
+def mainEscuela():
+    if request.method == "POST":
+        return registrarEscuela()
+    elif request.method == "GET":
+        return verEscuelas()
+    return {"error:" "la consulta no es valida"}, 500
+    
+
+def verEscuelas():
+    schoolcue = request.args.get('cue',default="", type="string")
+    schoolid = request.args.get('id',default=0, type="int")
+    localidad = request.args.get('localidad',default="", type="string")
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="reports",
+            port=3306
+        )
+        cursor = db.cursor()
+        query = "SELECT * FROM escuelas WHERE cue = %s OR  id_school = %s OR localidad = %s"
+        cursor.execute(query, (schoolcue,schoolid,localidad))
+
+        return cursor.fetchall(),202
+    
+    except mysql.connector.Error as expc:
+        return {"error":"Error al realizar la consulta: "+expc},403
+
+def registrarEscuela ():
+    school_data = request.json
+    nombre = school_data.get("nombre")
+    director = school_data.get("nombre")
+    calle = school_data.get("calle")
+    altura = school_data.get("altura")
+    localidad = school_data.get("localidad")
+    cue = school_data.get("cue")
+    lat = school_data.get("lat")
+    lng = school_data.get("lng")
+
+    if not nombre or not lat or not lng or not calle or not altura or not localidad or not cue:
+        return {"error": "Se requiere ingresar los campos"}, 400
+
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="reports",
+            port=3306
+        )
+        cursor = db.cursor()
+        query = "SELECT nombre FROM escuelas WHERE cue = %s"
+        cursor.execute(query, (cue))
+        existing_school = cursor.fetchone()
+
+        if existing_school:
+            return {"message": "La escuela ya existe"}, 200
+        query = "INSERT INTO `escuelas` (`id_escuela`, `nombre`, `director`, `calle`, `altura`, `cue`, `localidad`, `lat`, `lng`) VALUES (NULL, '%s', '%s', '%s', '%i', '%s', '%s', '%f', '%f');"
+        cursor.execute(query, (nombre,director,calle,altura,cue,localidad,lat,lng))
+        db.commit()
+        cursor.close()
+        db.close()
+        return {"message": "Escuela Registrada."}, 201
+    except mysql.connector.Error as excp:
+        print(f"Error registrando escuela: {excp}")
+        return {"error": "Error registrando escuela"}, 500
 
 def generate_random_token():
     """Genera un token aleatorio para la contraseña"""
